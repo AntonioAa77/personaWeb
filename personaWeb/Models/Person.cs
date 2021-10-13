32fb66c8f81b42d10f3ec0e3ef78ce67beab321f
Antonio <a2020115255@estudiantes.upsa.edu.bo>
2021-10-12 20:38:59 -0400

    Agregar archivos de proyecto.

---
+++ personaWeb/Models/Person.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace personaWeb.Models
+{
+    public enum SexType
+    {
+        Femenino = 0,
+        Masculino = 1,
+    }
+    public class Person
+    {
+        [Key]
+        public int PersonID { get; set; }
+        [Required]
+        [StringLength(60,MinimumLength = 5,ErrorMessage = "Por favor ingrese entre 5 a 60 caracteres")]
+        [Display(Name = "Nombre de la persona")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Sexo")]
+        public SexType Sex { get; set; }
+        [Display(Name = "Pasa Tiempo")]
+        public string Hobbie { get; set; }
+
+
+    }
+}
